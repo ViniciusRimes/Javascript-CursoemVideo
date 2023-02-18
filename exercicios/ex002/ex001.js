9c9835503ed6ecde40f4f980e9b6a322e20688e4
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2023-02-18 14:45:30 -0300

    segundo desafio

--- exercicios/ex002/ex001.js
+++ exercicios/ex002/ex001.js
@@ -3,20 +3,22 @@ function verificar()
     var anoAtual = new Date().getFullYear()
     var fAnoNasc = window.document.getElementById('inascimento')
     var res = window.document.querySelector('div#res')
+    var foto = window.document.getElementById("foto")
+    var fsex = window.document.getElementsByName('radsexo')
+    var idade = anoAtual - Number(fAnoNasc.value)
+    var genero = ''
 
     if(fAnoNasc.value.length == 0 || fAnoNasc.value > anoAtual)
     {
         window.alert("Digite um ano de nascimento válido e tente novamente!")
     }else{
-        var fsex = window.document.getElementsByName('radsexo')
-        var idade = anoAtual - Number(fAnoNasc.value)
-        var genero = ''
-        if(fsex[0].checked){
+        if(fsex[0].checked && idade < 6){
             genero = 'homem'
+            foto.src = "bebe-m.jpg"
         }
         else{
             genero = 'mulher'
         }
-        res.innerHTML = `Você é ${genero} e tem ${idade} anos de idade.`
     }
+        res.innerHTML = `Você é ${genero} e tem ${idade} anos de idade.`
 }
